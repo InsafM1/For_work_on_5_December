007e63c648e9710fca243b7d5cfb4f85f7992003
InsafM1 <muhamadiarovinsaf0@gmail.com>
2025-12-05 12:58:22 +0300

    Coding and corrected

--- practice.cpp
+++ practice.cpp
@@ -11,18 +11,23 @@ namespace topit
   };
   struct f_t { p_t aa, bb; };
   struct Dot: IDraw {
+    Dot(int x, int y);
+    explicit Dot(p_t dd);
     p_t begin() const override;
-    p_t next(p_t) const override; 
+    p_t next(p_t) const override;
+    p_t d; 
   };
 }
 int main()
 {
   using topit::p_t;
-  p_t a{1, 0}, b{1, 0};
-  std::cout << (a == b) << '\n';
-
-  
+  IDraw * p = new Dot(10, 10);
+  IDraw * p = new Dot(p_t{10, 10});
+  delete p;
+  delete pp;
 }
+topit::Dot::Dot(p_t dd): IDraw(), d{dd}
+{}
 
 bool topit::operator==(p_t a, p_t b)
 {
@@ -33,3 +38,21 @@ bool topit::operator!=(p_t a, p_t b)
 {
   return !(a == b);
 }
+
+topit::p_t topit::Dot::begin() const 
+{
+  return d;
+}
+
+topit::p_t topit::Dot::next(p_t prev) const 
+{
+  if (prev != begin){
+    throw std::logic_error("bad impl"); 
+  }
+  return d;
+}
+
+topit::Dot::Dot(int x, int y):
+ Idraw();
+ d{x, y}
+{}
